a92d00f7821b9df107c15d9b552e10ba1aa131db
Guilherme Latrova <hello@guilatrova.dev>
2021-12-19 19:55:29 -0300

    feat: add sign up link and clear menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -44,7 +44,7 @@ const Header = ({
       document.removeEventListener('click', clickOutside);
       closeMenu();
     };
-  });  
+  });
 
   const openMenu = () => {
     document.body.classList.add('off-nav-is-active');
@@ -66,7 +66,7 @@ const Header = ({
     if (!nav.current) return
     if (!isActive || nav.current.contains(e.target) || e.target === hamburger.current) return;
     closeMenu();
-  }  
+  }
 
   const classes = classNames(
     'site-header',
@@ -111,16 +111,16 @@ const Header = ({
                       'list-reset text-xs',
                       navPosition && `header-nav-${navPosition}`
                     )}>
-                    <li>
+                    {/* <li>
                       <Link to="#0" onClick={closeMenu}>Documentation</Link>
-                    </li>
+                    </li> */}
                   </ul>
                   {!hideSignin &&
                     <ul
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        <a href="https://profly.app/" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu} >Sign up</a>
                       </li>
                     </ul>}
                 </div>
