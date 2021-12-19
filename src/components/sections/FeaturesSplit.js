a7aeea514bed82a4d63b7da69424eb614a9c120c
Guilherme Latrova <hello@guilatrova.dev>
2021-12-19 19:41:20 -0300

    feat: add features

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -50,8 +50,8 @@ const FeaturesSplit = ({
   );
 
   const sectionHeader = {
-    title: 'Workflow that just works',
-    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+    title: 'Made to be easy',
+    paragraph: 'Insightful data to improve your decision-making.'
   };
 
   return (
@@ -67,14 +67,14 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  Simple workflow
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Data-driven insights
-                  </h3>
+                  Quick answers
+                </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                  “Where is my money?” should be an easy question to answer — now it is. <br />Overview of all your investiments right there. 👉
+                </p>
               </div>
               <div className={
                 classNames(
@@ -83,8 +83,8 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/features-split-image-01.png')}
-                  alt="Features split 01"
+                  src={require('./../../assets/images/feats/savings.png')}
+                  alt="All savings clear"
                   width={528}
                   height={396} />
               </div>
@@ -93,14 +93,14 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  Simple workflow
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Data-driven insights
+                  Data with context
                   </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                  Is the hype driving your decisions? Are you selling because of a sudden drop? <br /> 🟢 Buy and 🔴 Sell decisions spread over the chart.
+                </p>
               </div>
               <div className={
                 classNames(
@@ -109,8 +109,8 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/features-split-image-02.png')}
-                  alt="Features split 02"
+                  src={require('./../../assets/images/feats/stock-tesla-on-chart.png')}
+                  alt="Tesla stock with context"
                   width={528}
                   height={396} />
               </div>
@@ -119,14 +119,14 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  Simple workflow
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Data-driven insights
+                  “Emoji-driven” analysis
                   </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                  See how your emotions are affecting your decisions, and ultimately, learn from past mistakes.
+                </p>
               </div>
               <div className={
                 classNames(
@@ -135,7 +135,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/features-split-image-03.png')}
+                  src={require('./../../assets/images/feats/emotion-driven-analysis.png')}
                   alt="Features split 03"
                   width={528}
                   height={396} />
