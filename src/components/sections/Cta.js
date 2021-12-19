a76fa4f581bb700348ada4cc5a06114d9ecbf15d
Guilherme Latrova <hello@guilatrova.dev>
2021-12-19 20:38:08 -0300

    fix: add CTA button

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
 import Input from '../elements/Input';
+import Button from '../elements/Button';
 
 const propTypes = {
   ...SectionProps.types,
@@ -40,7 +41,7 @@ const Cta = ({
     topDivider && 'has-top-divider',
     bottomDivider && 'has-bottom-divider',
     split && 'cta-split'
-  );  
+  );
 
   return (
     <section
@@ -53,15 +54,13 @@ const Cta = ({
         >
           <div className="cta-slogan">
             <h3 className="m-0">
-              For previewing layouts and visual?
-              </h3>
+              Ready to be on top of your finances?
+            </h3>
           </div>
           <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your best email">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+            <Button tag="a" wideMobile href="https://profly.app/">
+              Get started
+            </Button>
           </div>
         </div>
       </div>
